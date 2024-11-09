ae3d63a346b59bc8627da76961c2d84efd23e4f5
kutalekj <jirka.kut@centrum.cz>
2024-11-09 14:51:21 +0100

    Move new full list of comps to settings

--- src/apifootball_model/main_apifootball.py
+++ src/apifootball_model/main_apifootball.py
@@ -15,62 +15,6 @@ from train import train
 
 global_instance = Global.get_instance()
 
-"""
-    {'id': 39, 'name': "Premier League", 'regular_round_keywords': ['Regular Season']},
-    {'id': 40, 'name': "Championship", 'regular_round_keywords': ['Regular Season']},
-    {'id': 41, 'name': "League One", 'regular_round_keywords': ['Regular Season']},
-    {'id': 42, 'name': "League Two", 'regular_round_keywords': ['Regular Season']},
-    {'id': 61, 'name': "Ligue 1", 'regular_round_keywords': ['Regular Season']},
-    {'id': 62, 'name': "Ligue 2", 'regular_round_keywords': ['Regular Season']},
-    {'id': 78, 'name': "Bundesliga", 'regular_round_keywords': ['Regular Season']},
-    {'id': 79, 'name': "2. Bundesliga", 'regular_round_keywords': ['Regular Season']},
-    {'id': 88, 'name': "Eredivisie", 'regular_round_keywords': ['Regular Season']},
-    {'id': 94, 'name': "Primeira Liga", 'regular_round_keywords': ['Regular Season']},
-    {'id': 106, 'name': "Ekstraklasa", 'regular_round_keywords': ['Regular Season']},  # POL
-    {'id': 119, 'name': "Superliga",
-     'regular_round_keywords': ['Regular Season', 'Championship Round', 'Relegation Round']},  # DEN
-    {'id': 135, 'name': "Serie A", 'regular_round_keywords': ['Regular Season']},
-    {'id': 136, 'name': "Serie B", 'regular_round_keywords': ['Regular Season']},
-    {'id': 140, 'name': "La Liga", 'regular_round_keywords': ['Regular Season']},
-    {'id': 141, 'name': "Segunda División", 'regular_round_keywords': ['Regular Season']},
-    {'id': 144, 'name': "Jupiler Pro League",
-     'regular_round_keywords': ['Regular Season', 'Championship Round', 'Conference League Play-off Group']},  # BEL
-    {'id': 179, 'name': "Premiership",
-     'regular_round_keywords': ['1st Phase', 'Championship Round', 'Relegation Round -']},  # SCO
-    {'id': 188, 'name': "A-League",
-     'regular_round_keywords': ['Regular Season', 'Elimination Finals', 'Semi-finals', 'Grand Final']},  # AUS
-    {'id': 203, 'name': "Süper Lig", 'regular_round_keywords': ['Regular Season']},  # TUR
-    {'id': 207, 'name': "Super League",
-     'regular_round_keywords': ['Regular Season', 'Championship Round', 'Relegation Round -']},  # SUI
-    {'id': 218, 'name': "Bundesliga",
-     'regular_round_keywords': ['Regular Season', 'Championship Round', 'Relegation Round -']},  # AUT
-    {'id': 307, 'name': "Pro League", 'regular_round_keywords': ['Regular Season']},  # SA
-    {'id': 323, 'name': "Super League",
-     'regular_round_keywords': ['Regular Season', 'Qualifying Finals', 'Championship -']},  # IND
-    {'id': 2, 'name': "UEFA Champions League", 'regular_round_keywords': []},
-    {'id': 3, 'name': "UEFA Europa League", 'regular_round_keywords': []},
-    {'id': 848, 'name': "UEFA Europa Conference League", 'regular_round_keywords': []},
-    {'id': 45, 'name': "FA Cup", 'regular_round_keywords': []},
-    {'id': 46, 'name': "EFL Trophy", 'regular_round_keywords': []},
-    {'id': 81, 'name': "DFB Pokal", 'regular_round_keywords': []},
-    {'id': 66, 'name': "Coupe de France", 'regular_round_keywords': []},
-    {'id': 137, 'name': "Coppa Italia", 'regular_round_keywords': []},
-    {'id': 143, 'name': "Copa del Rey", 'regular_round_keywords': []},
-    {'id': 90, 'name': "KNVB Beker", 'regular_round_keywords': []},  # NED
-    {'id': 96, 'name': "Taça de Portugal", 'regular_round_keywords': []},
-    {'id': 97, 'name': "Taça da Liga", 'regular_round_keywords': []},
-    {'id': 108, 'name': "Cup", 'regular_round_keywords': []},  # POL
-    {'id': 209, 'name': "Schweizer Cup", 'regular_round_keywords': []},
-    {'id': 206, 'name': "Cup", 'regular_round_keywords': []},  # TUR
-    {'id': 121, 'name': "DBU Pokalen", 'regular_round_keywords': []},  # DEN
-    {'id': 147, 'name': "Cup", 'regular_round_keywords': []},  # BEL
-    {'id': 181, 'name': "FA Cup", 'regular_round_keywords': []},  # SCO
-    {'id': 185, 'name': "League Cup", 'regular_round_keywords': []},  # SCO
-    {'id': 220, 'name': "Cup", 'regular_round_keywords': []},  # AUT
-    {'id': 504, 'name': "King's Cup", 'regular_round_keywords': []},  # SA
-    {'id': 874, 'name': "Australia Cup", 'regular_round_keywords': []}  # AUS
-"""
-
 # 1. Init comps and their seasons and rounds
 for comp in [
     {'id': 144, 'name': "Jupiler Pro League",
