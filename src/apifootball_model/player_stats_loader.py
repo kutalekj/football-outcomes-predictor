e74c9d3380ead97e6f3bd03b091630f298ae3e74
kutalekj <jirka.kut@centrum.cz>
2024-11-09 16:15:19 +0100

    Implement new logic for finding player row in CSV

--- src/apifootball_model/player_stats_loader.py
+++ src/apifootball_model/player_stats_loader.py
@@ -4,7 +4,7 @@ import csv
 import difflib
 from datetime import datetime
 
-NUM_FUZZY_MATCHES = 5
+NUM_FUZZY_MATCHES = 10
 FUZZY_CUTOFF = 0.2
 
 categories = {
@@ -21,42 +21,58 @@ categories = {
 def get_csv_file(match_datetime, directory_path):
     csv_files = glob.glob(os.path.join(directory_path, '*.csv'))
     csv_files_dates = []
+
     for csv_file in csv_files:
         filename = os.path.basename(csv_file)
+
         try:
             date_str = filename.replace('.csv', '')
             date = datetime.strptime(date_str, '%Y-%m-%d')
             csv_files_dates.append((csv_file, date))
         except ValueError:
             continue
+
     files_before_match = [(file, date) for (file, date) in csv_files_dates if date <= match_datetime]
     if not files_before_match:
         return None
+
     selected_file = max(files_before_match, key=lambda x: x[1])[0]
     return selected_file
 
 
-def find_player_row(player_name, csv_file):
+def find_player_row(full_player_name, date_of_birth, csv_file):
     # Open CSV with a corresponding historical version
     with open(csv_file, 'r', encoding='utf-8') as f:
         reader = csv.DictReader(f)
         players = []
         rows = []
         for row in reader:
-            name = row['name']
+            name = row['full_name']
             players.append(name)
             rows.append(row)
 
     # Get close matches of players' names
-    matches = difflib.get_close_matches(player_name, players, n=NUM_FUZZY_MATCHES, cutoff=FUZZY_CUTOFF)
+    matches = difflib.get_close_matches(full_player_name, players, n=NUM_FUZZY_MATCHES, cutoff=FUZZY_CUTOFF)
+    # TODO: Check if sorted by match probability (desc.)
 
-    # TODO: Add logic here
     if matches:
         matched_name = matches[0]
-        print(f'Name {player_name} matched to {matched_name}.')
+
         index = players.index(matched_name)
         player_row = rows[index]
-        return player_row
+
+        # If date of birth matches, return the most probable match immediately
+        dob_csv = datetime.strptime(rows[index]['dob'], '%Y-%m-%d')
+        if date_of_birth.date() == dob_csv.date():
+            print(f"dob match")
+            return player_row
+
+        else:
+            print(f'Name {full_player_name} matched to {matched_name}, but dates of birth not matching '
+                  f'({date_of_birth} vs. {dob_csv})...')
+            return player_row
+            # TODO: Might need to implement more complex logic here
+
     else:
         return None
 
@@ -65,35 +81,43 @@ def extract_stats(player_row):
     stats = {}
     for category, columns in categories.items():
         values = []
+
         for column in columns:
             value_str = player_row.get(column, '').strip()
             try:
                 value = int(value_str)
             except (ValueError, TypeError):
                 value = -1
+
             values.append(value)
+
         stats[category] = values
+
     return stats
 
 
-def get_player_stats(player_name, match_datetime_str, directory_path):
-    # TODO: Call this function for entire match so that CSV is chosen and opened (loaded all player rows) only once!
+def get_player_stats_for_team(team_roster_info, match_datetime_str, directory_path):
     match_datetime = datetime.strptime(match_datetime_str, '%Y-%m-%d')
     selected_csv = get_csv_file(match_datetime, directory_path)
 
-    # TODO: From here loop over both teams and all players
     if not selected_csv:
         stats_dict = {category: [-1]*len(columns) for category, columns in categories.items()}
         return False, stats_dict
-    player_row = find_player_row(player_name, selected_csv)
-    if not player_row:
-        stats_dict = {category: [-1]*len(columns) for category, columns in categories.items()}
-        return False, stats_dict
-    else:
-        stats = extract_stats(player_row)
-        return True, stats
+
+    # Loop over players in team roster
+    for player_info in team_roster_info:
+
+        # TODO: Continue here...
+        player_row = find_player_row(player_name, selected_csv)
+        if player_row is None:
+            stats_dict = {category: [-1]*len(columns) for category, columns in categories.items()}
+            return False, stats_dict
+        else:
+            stats = extract_stats(player_row)
+            return True, stats
 
 
+"""
 player_name = 'Erling Haaland'
 match_datetime_str = '2024-10-02'
 directory_path = 'C:\\Users\\kutalekj\\PycharmProjects\\sofifa-web-scraper\\output\\test'
@@ -107,3 +131,4 @@ if found:
 else:
     print("Player not found or stats not available.")
     print(player_stats)
+"""
