8a529ed0254b4de4921e3963a8b68ffa0445bf9d
kutalekj <jirka.kut@centrum.cz>
2025-03-04 15:33:12 +0100

    Add draft of comp IDs encoder training

--- src/apifootball_model/preseason_checklist.md
+++ src/apifootball_model/preseason_checklist.md
@@ -1,3 +1,4 @@
 
 ### Pre-season (2025/2026) necessary adjustments/checks
+- change `settings.LAST_SEASON`
 - check AF/FS teams matching in `assign_fs_team_id_team_name_by_comp_season` (`team.py`) - must be 100% accurate
---
+++ src/apifootball_model/train_compID_encoder.py
@@ -0,0 +1,25 @@
+import settings
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Embedding, Flatten, Dense, Concatenate, Activation
+from tensorflow.keras.models import Model
+
+comp_input = Input(shape=(1,), name='competition_id')
+
+comp_embedding = Embedding(input_dim=settings.NUM_REGULAR_COMPS, output_dim=5, name='competition_embedding')(comp_input)
+comp_embed_flat = Flatten()(comp_embedding)
+
+comp_embed_norm = Activation('sigmoid')(comp_embed_flat)  # normalize values to (0,1)
+
+# MLP
+x = Dense(32, activation='relu')(comp_embed_norm)
+x = Dense(16, activation='relu')(x)
+output = Dense(1, activation='sigmoid')(x)
+
+embedding_model = Model(
+    inputs=[comp_input],
+    outputs=output
+)
+embedding_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+embedding_model.summary()
+
+# TODO: get data and pass them to model training...
