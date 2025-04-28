ee3bb3872c8fee5b1947af00ef88501c5872dd39
kutalekj <jirka.kut@centrum.cz>
2025-04-28 14:42:51 +0200

    Update trained main model and records file and graph

--- src/apifootball_model/records_summary.py
+++ src/apifootball_model/records_summary.py
@@ -23,7 +23,7 @@ def plot_winning_graph(records_file):
     x_labels = df["match_start_datetime_utc"].dt.strftime("%Y-%m-%d")
 
     model_updates = [datetime(2025, 3, 27), datetime(2025, 3, 30), datetime(2025, 4, 4), datetime(2025, 4, 7),
-                     datetime(2025, 4, 11), datetime(2025, 4, 18), datetime(2025, 4, 21)]
+                     datetime(2025, 4, 11), datetime(2025, 4, 18), datetime(2025, 4, 21), datetime(2025, 4, 26)]
 
     # Plot
     plt.figure(figsize=(13, 8))
