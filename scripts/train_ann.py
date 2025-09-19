159e732b771df4c4352184072b3918cffa4b572d
kutalekj <jirka.kut@centrum.cz>
2025-09-19 12:33:59 +0200

    chore(scripts): add thin runners for train_ann and train_rnn

---
+++ scripts/train_ann.py
@@ -0,0 +1,3 @@
+﻿from football_outcomes.training.train_ann import main
+if __name__ == "__main__":
+    raise SystemExit(main())
---
+++ scripts/train_rnn.py
@@ -0,0 +1,3 @@
+﻿from football_outcomes.training.train_rnn import main
+if __name__ == "__main__":
+    raise SystemExit(main())
